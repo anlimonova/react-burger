ef34ce83b809e0ea3fcbfd7a469ff07bf2a608fb
annalimonova <annalimonovaa@yandex.ru>
2025-09-07 16:35:45 +0300

    feat: github pages deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 
 import { store } from './services/store';
 
 if (import.meta.env.MODE !== 'production') {
   (window as WindowWithStore).__APP_STORE__ = store;
 }
+import { HashRouter } from 'react-router-dom';
+
 import { App } from '@components/app/app';
 
 import type { WindowWithStore } from '@/global';
@@ -16,10 +17,10 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
